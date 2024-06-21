7e43784e7637d2962a9d50bd02a0afb717b0e4a9
nguyenthanh311003 <nguyenthanh311003@gmail.com>
2024-06-21 16:18:04 +0700

    implement sign in, sign up and forgot password screen

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,16 @@
 import AdminLayout from '~/components/Layouts/AdminLayout';
 import AdDashboard from '~/pages/AdDashboard';
+import ForgotPassword from '~/pages/ForgotPassword';
 import Home from '~/pages/Home';
+import SignInSide from '~/pages/SignInSide';
+import SignUp from '~/pages/SignUp';
 
 const publicRoutes = [
     { path: '/', component: Home, layout: AdminLayout },
     { path: '/admin/dashboard', component: AdDashboard, layout: AdminLayout },
+    { path: '/sign-in', component: SignInSide, layout: null },
+    { path: '/sign-up', component: SignUp, layout: null },
+    { path: '/forgot-password', component: ForgotPassword, layout: null },
 ];
 
 const privateRoutes = [];
