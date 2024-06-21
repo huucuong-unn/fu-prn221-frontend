e0c9664e72981c37002c3ba6ea4feca0e9af539c
nguyenthanh311003 <nguyenthanh311003@gmail.com>
2024-06-21 15:39:50 +0700

    implement admin dashboard screen

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
-import DefaultLayout from '~/components/Layouts/DefaultLayout';
+import AdminLayout from '~/components/Layouts/AdminLayout';
+import AdDashboard from '~/pages/AdDashboard';
 import Home from '~/pages/Home';
 
-const publicRoutes = [{ path: '/', component: Home, layout: DefaultLayout }];
+const publicRoutes = [
+    { path: '/', component: Home, layout: AdminLayout },
+    { path: '/admin/dashboard', component: AdDashboard, layout: AdminLayout },
+];
 
 const privateRoutes = [];
 
