a521b40e0028ffce98d598b07edb00c5cdf83da2
CuongLH <lehuucuong270603@gmail.com>
2024-07-26 12:20:55 +0700

    update ui check point

---
+++ src/api/RequestPromotionAPI.js
@@ -0,0 +1,24 @@
+import axiosClient from './AxiosClient';
+
+const RequestPromotionAPI = {
+    addAuthorizationHeader(config, includeAuthorization) {
+        if (includeAuthorization) {
+            const token = JSON.parse(localStorage.getItem('accessToken'));
+            config.headers = {
+                Authorization: `Bearer ${token}`,
+                ...config.headers,
+            };
+        }
+        return config;
+    },
+
+    getAll() {
+        return axiosClient.get(`/v1/request-promotions`);
+    },
+
+    create(data) {
+        return axiosClient.post('/v1/request-promotion/create', data);
+    }
+};
+
+export default RequestPromotionAPI;
